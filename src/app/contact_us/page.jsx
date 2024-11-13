5f49dfde0510edf1195f618669672066ea68f9e1
Mayowa Ayeni <mayhoral008@gmail.com>
2024-11-13 16:35:54 +0100

    Add padding on contact page

--- src/app/contact_us/page.jsx
+++ src/app/contact_us/page.jsx
@@ -5,14 +5,14 @@ import mobileImage from "../../assets/contactUs/contact-us-mobile.png";
 
 const ContactUs = () => {
   return (
-    <div className="mt-20">
+    <div className="mt-20 mb-20">
       <Image src={image} alt="gloryseed school contact us page" className="h-72 lg:block hidden"/>
       <Image src={mobileImage} alt="gloryseed school contact page lg:hidden" className="lg:hidden block"/>
       <section className="flex lg:flex-row justify-center items-center flex-col lg:w-[50%] w-[90%] mx-auto lg:gap-x-4 gap-y-4 lg:gap-y-0">
         <article className="border shadow-sm lg:w-[50%] w-full h-40 flex flex-col items-center justify-center text-center mt-10 lg:mt-0">
           <h2>Our Address</h2>
           <div>
-            <p>
+            <p className="px-2">
               Olorunsogo Bus Stop, Ganmo, Beside St. William Catholic Church,
               Ifelodun LGA, Kwara State, Nigeria.
             </p>
