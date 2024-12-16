22105c885149738e0583bcb6c646bdd57525e600
Mayowa Ayeni <mayhoral008@gmail.com>
2024-12-16 15:23:19 +0100

    Update signout modal, add animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ import Link from "next/link";
 import { useEffect } from "react";
 import Slideshow from "../components/slideshow";
 
-
 import hero_image_1 from "@/assets/landingPage/hero-images/hero-image-1.png";
 import hero_image_2 from "@/assets/landingPage/hero-images/hero-image-2.png";
 import mobile_slide_image_1 from "@/assets/landingPage/mobile-slide-images-1/image-1.png";
@@ -24,7 +23,6 @@ import unique_image_3 from "@/assets/landingPage/unique-image-3.png";
 
 import Aos from "aos";
 import "aos/dist/aos.css";
-
 
 export default function Home() {
   useEffect(() => {
@@ -48,9 +46,9 @@ export default function Home() {
           <span className="text-white">is Important and Thrives</span>
         </div>
         <Link href="/about_us">
-        <button className="w-40 h-10 bg-[#ECD337] text-lg text-gray-900 rounded-3xl">
-          Find out more
-        </button>
+          <button className="w-40 h-10 bg-[#ECD337] text-lg text-gray-900 rounded-3xl">
+            Find out more
+          </button>
         </Link>
       </div>
     );
@@ -68,26 +66,36 @@ export default function Home() {
       <section className="lg:grid grid-cols-2 mt-20 px-24 hidden">
         <article className="flex flex-col gap-y-10">
           <div className="flex flex-col text-6xl">
-        <span className="font-bold text-[#AA7529]">
-            A Learning <br/>
-            Citadel Where <br/>
-            Every Learner
-          </span>
-          <span className="text-gray-950">is Important <br/> and Thrives</span>
+            <span className="font-bold text-[#AA7529]">
+              A Learning <br />
+              Citadel Where <br />
+              Every Learner
+            </span>
+            <span className="text-gray-950">
+              is Important <br /> and Thrives
+            </span>
           </div>
           <p>
-            Welcome to Gloryseed Schools, a nurturing and dynamic environment where young minds begin their educational journey. At our School, we provide a strong foundation in academic and social skills, fostering creativity, curiosity, and a love for learning.
-
+            Welcome to Gloryseed Schools, a nurturing and dynamic environment
+            where young minds begin their educational journey. At our School, we
+            provide a strong foundation in academic and social skills, fostering
+            creativity, curiosity, and a love for learning.
           </p>
           <Link href="/about_us">
-          <button className="w-40 h-10 bg-[#ECD337] text-lg text-gray-900 rounded-3xl">
-          Find out more
-        </button>
+            <button className="w-40 h-10 bg-[#ECD337] text-lg text-gray-900 rounded-3xl">
+              Find out more
+            </button>
           </Link>
         </article>
 
         <article className="flex flex-col gap-y-4">
-          <div className="flex flex-row gap-x-2">
+          <div
+            className="flex flex-row gap-x-2"
+            data-aos="fade-down"
+            data-aos-easing="ease-out"
+            data-aos-duration="1200"
+            data-aos-once="true"
+          >
             <div>
               <Image src={hero_image_1} alt="cultural-day" />
             </div>
@@ -95,8 +103,18 @@ export default function Home() {
               <Image src={hero_image_2} alt="cultural-day" />
             </div>
           </div>
-          <div>
-            <Image src={header_img} alt="quiz-competition" height={50} width={600}/>
+          <div
+            data-aos="zoom-in"
+            data-aos-easing="ease-out"
+            data-aos-duration="1200"
+            data-aos-once="true"
+          >
+            <Image
+              src={header_img}
+              alt="quiz-competition"
+              height={50}
+              width={600}
+            />
           </div>
         </article>
       </section>
@@ -118,93 +136,120 @@ export default function Home() {
       >
         <h2>Portals</h2>
         <article className="lg:flex-row  flex flex-col justify-center items-center lg:justify-between gap-y-8 lg:gap-y-0">
-
-        <div className="flex text-center lg:text-start items-center flex-col lg:items-start lg:w-[30%] gap-y-4">
-          <h2 className="font-bold text-2xl">Assignments</h2>
-          <p className="px-6 lg:px-0 font-light">
-            Click the check assignments button below to check your child/ward’s
-            assignments
-          </p>
-          <Link href="/portals/assignments">
-          <button className="rounded-xl px-2 justify-center flex items-center bg-[#ECD337] w-44 h-8 ">
-            Check assignments
-          </button>
-          </Link>
-        </div>
-        <div className="flex text-center lg:text-start lg:items-start items-center flex-col  lg:w-[30%] gap-y-4">
-          <h2 className="font-bold text-2xl">Results</h2>
-          <p className="px-6 lg:px-0 font-light">
-            Click the check results button below to check your child/ward’s
-            results{" "}
-          </p>
-          <Link href="/portals/result">
-          <button className="rounded-xl  flex items-center bg-white text-gray-900 w-40 h-8 justify-center">
-            Check result
-          </button>
-          </Link>
-        </div>
+          <div className="flex text-center lg:text-start items-center flex-col lg:items-start lg:w-[30%] gap-y-4">
+            <h2 className="font-bold text-2xl">Assignments</h2>
+            <p className="px-6 lg:px-0 font-light">
+              Click the check assignments button below to check your
+              child/ward’s assignments
+            </p>
+            <Link href="/portals/assignments">
+              <button className="rounded-xl px-2 justify-center flex items-center bg-[#ECD337] w-44 h-8 ">
+                Check assignments
+              </button>
+            </Link>
+          </div>
+          <div className="flex text-center lg:text-start lg:items-start items-center flex-col  lg:w-[30%] gap-y-4">
+            <h2 className="font-bold text-2xl">Results</h2>
+            <p className="px-6 lg:px-0 font-light">
+              Click the check results button below to check your child/ward’s
+              results{" "}
+            </p>
+            <Link href="/portals/result">
+              <button className="rounded-xl  flex items-center bg-white text-gray-900 w-40 h-8 justify-center">
+                Check result
+              </button>
+            </Link>
+          </div>
         </article>
       </section>
       <section className="text-center mt-10">
         <h2>We are unique</h2>
         <div className="lg:grid lg:grid-cols-3 grid-rows-3 w-full lg:h-[600px]">
-        <article>
-          <Image src={unique_image_1} alt="gloryseed-unique-features" className=" ml-1 lg:ml-0"/>
-          <p className="text-center px-4">
-            We are equipped with dedicated and supportive staff members and a
-            proactive management ensuring our learners enjoy quality educational
-            and social experience
-          </p>
-        </article>
-        <article className="">
-          <Image src={unique_image_2} alt="gloryseed-unique-features"  className="ml-1 lg:ml-0"/>
-          <p className="text-center px-4 ">
-            We not only build our learners to be the best in academics but also
-            equip them with knowledge of cultural diversity making them
-            adaptable anywhere in the world
-          </p>
-        </article>
-        <article>
-          <Image src={unique_image_3} alt="gloryseed-unique-features"  className="ml-1 lg:ml-0"/>
-          <p className="text-center px-4 h-20">
-            Vocational skill is an essential part of the learning process at our
-            school and all learners are encouraged to learn a vocation that
-            interests them.
-          </p>
-        </article>
+          <article
+            data-aos="fade-up"
+            data-aos-easing="ease-out"
+            data-aos-duration="1200"
+            data-aos-once="true"
+          >
+            <Image
+              src={unique_image_1}
+              alt="gloryseed-unique-features"
+              className=" ml-1 lg:ml-0"
+            />
+            <p className="text-center px-4">
+              We are equipped with dedicated and supportive staff members and a
+              proactive management ensuring our learners enjoy quality
+              educational and social experience
+            </p>
+          </article>
+          <article
+            className=""
+            data-aos="fade-up"
+            data-aos-easing="ease-out"
+            data-aos-duration="1200"
+            data-aos-once="true"
+          >
+            <Image
+              src={unique_image_2}
+              alt="gloryseed-unique-features"
+              className="ml-1 lg:ml-0"
+            />
+            <p className="text-center px-4 ">
+              We not only build our learners to be the best in academics but
+              also equip them with knowledge of cultural diversity making them
+              adaptable anywhere in the world
+            </p>
+          </article>
+          <article
+            data-aos="fade-up"
+            data-aos-easing="ease-out"
+            data-aos-duration="1200"
+            data-aos-once="true"
+          >
+            <Image
+              src={unique_image_3}
+              alt="gloryseed-unique-features"
+              className="ml-1 lg:ml-0"
+            />
+            <p className="text-center px-4 h-20">
+              Vocational skill is an essential part of the learning process at
+              our school and all learners are encouraged to learn a vocation
+              that interests them.
+            </p>
+          </article>
         </div>
-
       </section>
 
       <section className="flex flex-col justify-center items-center  text-center bg-[#212121]  mt-10 text-white gap-y-4">
-          
         <h2 className="">Admissions</h2>
         <article className="flex flex-col  lg:flex lg:flex-row lg:justify-between  lg:px-20 lg:items-center min-h-max gap-x-2 w-full">
+          <div className="flex items-center lg:items-start lg:text-start h-full flex-col justify-center gap-y-4 lg:w-[50%]">
+            <p className="text-3xl px-2 font-bold">
+              Looking to enrol <br className="hidden lg:block" /> your
+              child/ward in a school <br className="hidden lg:block" /> embodied
+              with uniqueness, creativity and academic excellence?
+            </p>
+            <p className="text-[#ECD337] px-2">
+              Gloryseed Schools is not just the best but also the right option
+              for your Children/Wards
+            </p>
 
-        <div className="flex items-center lg:items-start lg:text-start h-full flex-col justify-center gap-y-4 lg:w-[50%]">
-        <p className="text-3xl px-2 font-bold">
-          Looking to enrol <br className="hidden lg:block"/> your child/ward in a school <br className="hidden lg:block"/> embodied with uniqueness,
-          creativity and academic excellence?
-        </p>
-        <p className="text-[#ECD337] px-2">
-          Gloryseed Schools is not just the best but also the right option for
-          your Children/Wards
-        </p>
+            <Link href="/admissions">
+              <button className="w-40 h-10 bg-[#ECD337] text-lg text-gray-900 rounded-3xl ">
+                Enroll Now
+              </button>
+            </Link>
+          </div>
 
-        <Link href="/admissions">
-        <button className="w-40 h-10 bg-[#ECD337] text-lg text-gray-900 rounded-3xl ">
-          Enroll Now
-        </button>
-        </Link>
-        </div>
-        
-      <div className="lg:w-[50%] lg:ml-10 h-96 rounded-t-sm lg:border-t-0 mb-10 mt-4 lg:mt-0 overflow-hidden">
-        <Slideshow images={images_1} interval={5000} imagePosition="contain"/>
-      </div>
-
+          <div className="lg:w-[50%] lg:ml-10 h-96 rounded-t-sm lg:border-t-0 mb-10 mt-4 lg:mt-0 overflow-hidden">
+            <Slideshow
+              images={images_1}
+              interval={5000}
+              imagePosition="contain"
+            />
+          </div>
         </article>
       </section>
-
     </main>
   );
 }
--- src/components/SignoutModal.jsx
+++ src/components/SignoutModal.jsx
@@ -30,7 +30,7 @@ export default function SignoutModal() {
     return (
       <div className=" fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-[rgba(0,0,0,0.6)] z-50 ">
         <div className=" rounded-md  bg-white lg:w-[30%] w-[70%] lg:h-[40%] h-[30%] border flex flex-col justify-between items-center  backdrop-blur-md  py-4">
-          <CiSquareInfo className="text-xl" />
+          <CiSquareInfo className="text-3xl text-[#AA7529]" />
           <p className="px-4 text-center text-xl font-medium lg:text-base">
             Are you sure you want to sign out?
           </p>
@@ -40,10 +40,9 @@ export default function SignoutModal() {
               className="rounded-md px-2 h-8 w-20 border border-gray-950 text-black"
               onClick={handleClose}
             >
-              Close
+              No
             </button>
             <button
-              
               className="rounded-md px-2 h-8 w-20 bg-black text-white"
               onClick={handleLogout}
             >
